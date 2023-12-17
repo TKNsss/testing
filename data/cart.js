ada21c6a758ef0193fe08e7fea54b4a411f020b4
Mr.Kim <nguyenkimto2004@gmail.com>
2023-12-17 17:52:44 +0700

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -6,7 +6,7 @@ export const cart = [
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1
-  },
+  }
 ];
 
 // notice that data-product-name -> product-name convert into productName (kebab-case to camel case)
@@ -33,4 +33,16 @@ export function addToCart(productId) {
       quantity
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
